f0a17ee11a4833c60087a785ee2b2c3f768f5b07
Nnenanya Obinna <obinnaeye@yahoo.com>
2017-03-11 09:20:00 +0100

    add aritGeo, all test cases pass

--- app/index.js
+++ app/index.js
@@ -33,5 +33,48 @@ module.exports = {
             }
             return true;
         }
+    },
+
+    aritGeo: function(arr){
+        if(!Array.isArray(arr)){
+            return "Invalid input! aritGeo accepts only an array as a parameter";
+        }else if(arr.length === 0){
+            return 0;
+        }else{
+            if(arr.length === 1){
+                return -1;
+            }else if(!checkArit(arr) && !checkGeo(arr)){
+                return -1;
+            }else if(checkArit(arr)){
+                return "Arithmetic";
+            }else{
+                return "Geometric";
+            }
+
+        }
+
+        function checkArit(a){
+            var len = a.length,
+                commonDiff = a[1] - a[0];
+            while(len >1){
+                if(a[len-1] - a[len-2] !== commonDiff){
+                    return false;
+                }
+                len--;
+            }
+            return true;
+        }
+
+        function checkGeo(a){
+            var len = a.length,
+                commonRatio = a[1] / a[0];
+            while(len >1){
+                if(a[len-1] / a[len-2] !== commonRatio){
+                    return false;
+                }
+                len--;
+            }
+            return true;
+        }
     }
 }
